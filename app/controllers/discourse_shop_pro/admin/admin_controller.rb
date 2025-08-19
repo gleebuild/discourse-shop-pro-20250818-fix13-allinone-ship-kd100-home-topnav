8f87d7cd2eef623eaa2dc6e00c1e55bbd6351138
gleebuild <gleebuild@gmail.com>
2025-08-19 13:00:14 +0000

    Delete app directory

--- app/controllers/discourse_shop_pro/admin/admin_controller.rb
+++
@@ -1,7 +0,0 @@
-class DiscourseShopPro::Admin::AdminController < ::Admin::AdminController; def bootstrap; render_json_dump({ok:true}); end; end
-
-def seed_nav
-  guardian.ensure_admin
-  ok = ::DiscourseShopPro::NavSeeder.ensure!
-  render json: { success: ok }
-end
