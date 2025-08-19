a682ea27e75e9880282dc9dd07ad0346a0e0040b
gleebuild <gleebuild@gmail.com>
2025-08-19 07:37:28 +0000

    Update logistics_controller.rb

--- app/controllers/discourse_shop_pro/logistics_controller.rb
+++ app/controllers/discourse_shop_pro/logistics_controller.rb
@@ -5,7 +5,7 @@ module ::DiscourseShopPro
     skip_before_action :check_xhr, :redirect_to_login_if_required, :ensure_logged_in, raise: false
     protect_from_forgery with: :null_session
 
-    # 快递 100 异步回调占位
+    # 快递100回调占位
     def kd100_notify
       render plain: "ok"
     end
