06e383efcdd2b25b86f36a8831e80551b23909b3
gleebuild <gleebuild@gmail.com>
2025-08-19 07:34:49 +0000

    Update engine.rb

--- lib/discourse_shop_pro/engine.rb
+++ lib/discourse_shop_pro/engine.rb
@@ -4,6 +4,5 @@ module ::DiscourseShopPro
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace DiscourseShopPro
-    # 这里不画 engine 路由，路由已在 plugin.rb 里 append 了
   end
 end
